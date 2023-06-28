d1ba447a63a33848b6d8072a36e7536923a6cc76
Ika1009 <ilija.doncic@yahoo.com>
2023-06-28 15:33:39 +0200

    dizajn, samo heder da se promeni boja i da bude visi

--- Form1.cs
+++ Form1.cs
@@ -63,7 +63,6 @@ namespace Image_Editing_app
             dataGridView1.Columns.Add(visibleColumn);
             dataGridView1.CellValueChanged += dataGridView1_CellValueChanged;
             dataGridView1.CurrentCellDirtyStateChanged += dataGridView1_CurrentCellDirtyStateChanged;
-            dataGridView1.CellFormatting += dataGridView1_CellFormatting;
 
 
             // Set the DataSource to the BindingList
@@ -167,23 +166,6 @@ namespace Image_Editing_app
             }
         }
 
-        private void dataGridView1_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
-        {
-            // Check if the current cell is in the selected row
-            if (e.RowIndex >= 0 && dataGridView1.Rows[e.RowIndex].Selected)
-            {
-                // Set custom selection colors
-                e.CellStyle.SelectionBackColor = Color.Red; // Change to the desired background color
-                e.CellStyle.SelectionForeColor = Color.White; // Change to the desired foreground color
-            }
-            else
-            {
-                // Reset the cell style for non-selected cells
-                e.CellStyle.SelectionBackColor = dataGridView1.DefaultCellStyle.SelectionBackColor;
-                e.CellStyle.SelectionForeColor = dataGridView1.DefaultCellStyle.SelectionForeColor;
-            }
-        }
-
         private void dataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
         {
             // Check if the modified cell belongs to the visibleColumn
@@ -282,7 +264,7 @@ namespace Image_Editing_app
 
             if (currentlySelectedButton == toolStripButton12)
             {
-                g.DrawLine(new Pen(new SolidBrush(Color.Blue), 5), new Point(x, y), new Point(lx, ly));
+                g.DrawLine(new Pen(new SolidBrush(Color.White), 5), new Point(x, y), new Point(lx, ly));
                 g.Dispose();
                 SelektujIliDeselektuj(toolStripButton12);
             }
@@ -296,7 +278,7 @@ namespace Image_Editing_app
 
                 if (points.Count == 2)
                 {
-                    g.DrawLine(new Pen(Color.Black, 3), points.First().X, points.First().Y, points.Last().X, points.Last().Y);
+                    g.DrawLine(new Pen(Color.White, 3), points.First().X, points.First().Y, points.Last().X, points.Last().Y);
                 }
             }
         }
