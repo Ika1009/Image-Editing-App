94e18292f89b9f84fa61774f84f91d193b1deb76
Ika1009 <ilija.doncic@yahoo.com>
2023-08-14 11:14:33 +0200

    Fixed the importing

--- ImportImage.Designer.cs
+++ ImportImage.Designer.cs
@@ -68,6 +68,7 @@
             this.button1.TabIndex = 2;
             this.button1.Text = "Select";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Import);
             // 
             // label1
             // 
